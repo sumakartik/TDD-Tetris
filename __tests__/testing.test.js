64b792484d82f919f88ec5c716e016db56c035c6
James Kelley <james.a.kelley.mars@gmail.com>
2022-02-03 20:47:19 -0700

    adjust testing tests of normalize function to jest syntax

---
+++ __tests__/testing.test.js
@@ -0,0 +1,9 @@
+import { normalize } from "./testing.mjs";
+
+it("normalize", () => {
+  expect(normalize("")).toEqual("\n");
+  expect(normalize("  x  ")).toEqual("x\n");
+  expect(normalize("   x\n   x")).toEqual("x\nx\n");
+  expect(normalize("   x\n   x\n")).toEqual("x\nx\n");
+  expect(normalize("\n   x\n   x")).toEqual("x\nx\n");
+});
--- __tests__/testing.test.mjs
+++
@@ -1,9 +0,0 @@
-import { normalize } from "./testing.mjs";
-
-it("normalize", () => {
-  expect(normalize("")).to.equal("\n");
-  expect(normalize("  x  ")).to.equal("x\n");
-  expect(normalize("   x\n   x")).to.equal("x\nx\n");
-  expect(normalize("   x\n   x\n")).to.equal("x\nx\n");
-  expect(normalize("\n   x\n   x")).to.equal("x\nx\n");
-});
